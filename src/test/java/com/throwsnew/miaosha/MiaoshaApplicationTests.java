0d9a272cb8531b266c696f1a82a3f9fac2016a4f
xianfengsong <songx@q.com>
2018-09-27 20:35:14 +0800

    测试用例

--- src/test/java/com/throwsnew/miaosha/MiaoshaApplicationTests.java
+++ src/test/java/com/throwsnew/miaosha/MiaoshaApplicationTests.java
@@ -3,27 +3,40 @@ package com.throwsnew.miaosha;
 import com.throwsnew.miaosha.dao.mapper.ItemMapper;
 import com.throwsnew.miaosha.dao.model.ItemDO;
 import com.throwsnew.miaosha.dao.model.ItemDOCriteria;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MiaoshaApplicationTests {
-//    @Autowired
-//    private ItemMapper itemMapper;
+
+    @Autowired
+    private ItemMapper itemMapper;
 
     @Test
-    public void contextLoads() {
-//        ItemDOCriteria criteria=new ItemDOCriteria();
-//        criteria.createCriteria().andIdIsNotNull();
-//        List<ItemDO> itemDOList = itemMapper.selectByExample(criteria);
-//        System.out.println(itemDOList.stream().map(ItemDO::toString).collect(Collectors.joining(",")));
+    public void insert() {
+        ItemDO itemDO = new ItemDO();
+        itemDO.setAmount(1L);
+        itemDO.setCategory("phone");
+        itemDO.setDescription("desc");
+        itemDO.setName("test");
+        itemDO.setPrice(10000);
+        itemDO.setUpdateTime(new Date());
+        itemMapper.insert(itemDO);
+    }
+    @Test
+    public void select() {
+        ItemDOCriteria criteria = new ItemDOCriteria();
+        criteria.createCriteria().andIdIsNotNull();
+        List<ItemDO> itemDOList = itemMapper.selectByExample(criteria);
+        System.out.println(
+                itemDOList.stream().map(ItemDO::toString).collect(Collectors.joining(",")));
     }
 
 }
